b5c1f9d1a329499304861fd4ae621d79265e9b0d
Yina Zhu <yinagit@gmail.com>
2018-06-11 04:38:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    puts params
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+    @user= User.find_by(email:params[:email], password:params[:password])
     redirect '/users/home'
   end
 
